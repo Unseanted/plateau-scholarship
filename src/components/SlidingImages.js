cec66547220d6f811b5e71bc1c4747c53bd1d630
unseanted <sinbad4u007@yahoo.com>
2025-02-28 15:28:41 +0100

    changes to the indexjs

--- src/components/SlidingImages.js
+++ src/components/SlidingImages.js
@@ -1,30 +1,33 @@
 import { useEffect, useState } from 'react';
+import Image from 'next/image';
 
 export default function SlidingImages() {
-    const images = [
-        "/images/student1.jpg",
-        "/images/student2.jpg",
-        "/images/student3.jpg"
-    ];
-    const [currentImage, setCurrentImage] = useState(0);
+	const images = [
+		"/images/student1.jpg",
+		"/images/student2.jpg",
+		"/images/student3.jpg"
+	];
+	const [currentImage, setCurrentImage] = useState(0);
 
-    useEffect(() => {
-        const interval = setInterval(() => {
-            setCurrentImage((prev) => (prev + 1) % images.length);
-        }, 3000);
-        return () => clearInterval(interval);
-    }, []);
+	useEffect(() => {
+		const interval = setInterval(() => {
+			setCurrentImage((prev) => (prev + 1) % images.length);
+		}, 3000);
+		return () => clearInterval(interval);
+	}, []);
 
-    return (
-        <div className="relative w-full h-96 flex justify-center items-center overflow-hidden">
-            {images.map((img, index) => (
-                <img
-                    key={index}
-                    src={img}
-                    className={`absolute w-full h-full object-cover transition-opacity duration-1000 ${index === currentImage ? 'opacity-100' : 'opacity-0'}`}
+	return (
+		<div className="relative w-full flex justify-center my-6">
+            <div className="relative w-[600px] h-[300px] overflow-hidden rounded-lg shadow-lg">
+                <Image 
+                    src={images[currentImage]} 
                     alt="Student Image"
+                    width={600}  
+                    height={300}
+                    objectFit="cover" 
+                    className="transition-transform duration-1000 ease-in-out rounded-lg"
                 />
-            ))}
+            </div>
         </div>
-    );
+        );
 }
