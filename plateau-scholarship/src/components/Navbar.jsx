e05bdffaa6dd414a273e7fb1128c16d7f0a4c817
unseanted <sinbad4u007@yahoo.com>
2025-03-28 18:34:40 +0100

    hover effects on navbar

--- plateau-scholarship/src/components/Navbar.jsx
+++ plateau-scholarship/src/components/Navbar.jsx
@@ -17,13 +17,14 @@ const Navbar = () => {
 
 			{/* Buttons on the Right */}
 			<div className="space-x-2">
-				<button className="px-4 py-2 bg-yellow-500 text-white rounded hover:bg-yellow-600">
+				<button className="px-4 py-2 bg-yellow-500 text-white rounded hover:bg-yellow-600 transition-opacity duration-300 hover:opacity-75">
 					Log In
 				</button>
-				<button className="px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600">
+				<button className="px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600 transition-opacity duration-300 hover:opacity-75">
 					Register
 				</button>
 			</div>
+
 		</nav>
 	);
 };
